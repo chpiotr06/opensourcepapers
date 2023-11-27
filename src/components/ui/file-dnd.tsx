fdc80d4aa96c9adaf7d2310751081762670258e8
chpiotr06 <chpiotr06@gmail.com>
2023-11-27 21:38:56 +0100

    feat: implement deleting uploaded files

--- src/components/ui/file-dnd.tsx
+++ src/components/ui/file-dnd.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { Upload } from 'lucide-react'
+import { Upload, X } from 'lucide-react'
+import { useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { Typography } from '@/components/ui/typography'
 import { useToast } from '@/components/ui/use-toast'
@@ -13,13 +14,16 @@ const MAX_FILE_SIZE = 30_000_000
 type FileDragAndDropProps = {
   folderName: string
   fileName: string
+  bucketName?: string
 }
 
-export const FileDragAndDrop = ({ folderName, fileName }: FileDragAndDropProps) => {
+export const FileDragAndDrop = ({ folderName, fileName, bucketName = 'public_bucket' }: FileDragAndDropProps) => {
+  const [file, setFile] = useState<File | null>()
   const { toast } = useToast()
   const supabase = createClient()
   const onDropHandler = async (acceptedFile: File[]) => {
-    const { error } = await supabase.storage.from('example_bucket').upload(`${folderName}/${fileName}`, acceptedFile[0])
+    setFile(acceptedFile[0])
+    const { error } = await supabase.storage.from(bucketName).upload(`${folderName}/${fileName}`, acceptedFile[0])
     if (!error) {
       toast({
         variant: 'success',
@@ -36,7 +40,28 @@ export const FileDragAndDrop = ({ folderName, fileName }: FileDragAndDropProps) 
       })
     }
   }
-  const { acceptedFiles, isFocused, isFileDialogActive, isDragActive, getRootProps, getInputProps } = useDropzone({
+
+  const onDeleteHandler = async () => {
+    setFile(null)
+    const { error } = await supabase.storage.from(bucketName).remove([`${folderName}/${fileName}`])
+    if (!error) {
+      toast({
+        variant: 'success',
+        duration: 5000,
+        title: 'Plik usunięto z serwera poprawnie',
+      })
+    } else {
+      toast({
+        variant: 'destructive',
+        duration: 5000,
+        title: 'Błędny email lub hasło',
+        description:
+          'Pliku nie udało sie usunąć z serwera. Sprobuj jeszcze raz. Jesli problem bedzie się powtarzał, skontaktuj się z nami',
+      })
+    }
+  }
+
+  const { isFocused, isFileDialogActive, isDragActive, getRootProps, getInputProps } = useDropzone({
     maxFiles: 1,
     maxSize: MAX_FILE_SIZE,
     onDrop: onDropHandler,
@@ -62,23 +87,23 @@ export const FileDragAndDrop = ({ folderName, fileName }: FileDragAndDropProps) 
           Maksymalny rozmiar to 30 MB
         </Typography>
       </div>
-      {acceptedFiles &&
-        acceptedFiles.map((file) => (
-          <div
-            key={file.name}
-            className='border-dominant bg-main-200 flex w-full flex-col rounded border pb-1 pl-2 pr-0 pt-2'
-          >
-            <div className='flex items-center'>
-              <div className='flex w-[80%] items-center gap-2'>
-                <Upload />
-                <Typography variant='p-12-500'>{file.name}</Typography>
-              </div>
-              <div className='flex w-[20%] justify-center'>
-                <Typography variant='p-10-400'>{filesizeSuffix(file.size)}</Typography>
-              </div>
+      {file && (
+        <div
+          key={file.name}
+          className='border-dominant bg-main-200 flex w-full flex-col rounded border pb-1 pl-2 pr-0 pt-2'
+        >
+          <div className='flex items-center'>
+            <div className='flex w-[80%] items-center gap-2'>
+              <Upload />
+              <Typography variant='p-12-500'>{file.name}</Typography>
+            </div>
+            <div className='flex w-[20%] items-center justify-center gap-2'>
+              <Typography variant='p-10-400'>{filesizeSuffix(file.size)}</Typography>
+              <X onClick={onDeleteHandler} className='cursor-pointer' />
             </div>
           </div>
-        ))}
+        </div>
+      )}
     </div>
   )
 }
